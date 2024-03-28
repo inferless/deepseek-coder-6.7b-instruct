b5066de191045510593e0cccb6043b6cd7a7badb
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-28 19:04:30 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -3,6 +3,6 @@ INPUT_SCHEMA = {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["What is Quantum Computing?"]
+        'example': ["Write a python Hello World code."]
     }
 }
